fe13793a9065deea51839cc7b8be0c58a8741bb5
Abhishek Vijeev <abhishek.vijeev@gmail.com>
2024-12-23 19:16:50 -0500

    Read SIFT_1M dataset

--- shinx/HNSWIndex.h
+++ shinx/HNSWIndex.h
@@ -2,10 +2,13 @@
 #define SHINX_HNSW_INDEX_H_
 
 #include <cstdint>
+#include <memory>
+#include <vector>
 
 namespace shinx {
 
-// Implementation of the Hierarchical Navigable Small World graph:
+// Implementation of the Hierarchical Navigable Small World (HNSW)
+// algorithm based on:
 // 
 // Malkov, Yu A., and Dmitry A. Yashunin. "Efficient and robust
 // approximate nearest neighbor search using hierarchical navigable
@@ -16,20 +19,27 @@ class HNSWIndex
   public:
 
   HNSWIndex();
-  void Insert();
+  void Insert(const std::vector<float>& embedding);
   void SearchLayer();
   void SelectNeighbours();
   void KNNSearch();
 
   private:
 
-  uint32_t entry_point_;
+  struct HNSWNode
+  {
+    std::vector<float> embedding;
+    std::vector<std::shared_ptr<HNSWNode>> neighbours;
+    std::shared_ptr<HNSWNode> skip_link;
+  };
+
+  std::shared_ptr<HNSWNode> entry_point_;
+
   uint32_t highest_level_;
   uint32_t expansion_factor_construction_;
   uint32_t level_generation_normalization_factor_;
   uint32_t established_connections_;
   uint32_t max_connections_;
-
 };
 
 } // namespace shinx
--- shinx/runner.cc
+++ shinx/runner.cc
@@ -1,7 +1,84 @@
 #include "HNSWIndex.h"
 
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+std::vector<std::vector<float>> fvecs_read(const std::string& file_path)
+{
+  std::vector<std::vector<float>> data;
+  std::ifstream file(file_path, std::ios::binary);
+  if (!file) {
+    throw std::runtime_error("Unable to open file" + file_path);
+  }
+
+  while (!file.eof()) {
+    int dim;
+    file.read(reinterpret_cast<char*>(&dim), sizeof(int));
+    if (file.eof()) {
+      break;
+    }
+
+    std::vector<float> vec(dim);
+    file.read(reinterpret_cast<char*>(vec.data()), dim * sizeof(float));
+    if (file.eof()) {
+      break;
+    }
+    data.push_back(std::move(vec));
+  }
+
+  return data;
+}
+
+std::vector<std::vector<int>> ivecs_read(const std::string& file_path)
+{
+  std::vector<std::vector<int>> data;
+  std::ifstream file(file_path, std::ios::binary);
+  if (!file) {
+    throw std::runtime_error("Unable to open file" + file_path);
+  }
+
+  while (!file.eof()) {
+    int dim;
+    file.read(reinterpret_cast<char*>(&dim), sizeof(int));
+    if (file.eof()) {
+      break;
+    }
+
+    std::vector<int> vec(dim);
+    file.read(reinterpret_cast<char*>(vec.data()), dim * sizeof(int));
+    if (file.eof()) {
+      break;
+    }
+    data.push_back(std::move(vec));
+  }
+
+  return data;
+}
+
 int main(int argc, char** argv)
 {
-    shinx::HNSWIndex h;
-    return 0;
+  std::string sift_base_path = "/home/avijeev/sift/sift_base.fvecs";
+  std::string sift_groundtruth_path = "/home/avijeev/sift/sift_groundtruth.ivecs";
+  std::string sift_learn_path = "/home/avijeev/sift/sift_learn.fvecs";
+  std::string sift_query_path = "/home/avijeev/sift/sift_query.fvecs";
+
+  std::vector<std::vector<float>> sift_base = fvecs_read(sift_base_path);
+  std::vector<std::vector<int>> sift_groundtruth = ivecs_read(sift_groundtruth_path);
+  std::vector<std::vector<float>> sift_learn = fvecs_read(sift_learn_path);
+  std::vector<std::vector<float>> sift_query = fvecs_read(sift_query_path);
+
+  // Print first base vector
+  // std::copy(sift_base[0].begin(), sift_base[0].end(), std::ostream_iterator<float>(std::cout, " "));
+  // std::cout << std::endl;
+
+  // Print first groundtruth vector
+  // std::copy(sift_groundtruth[0].begin(), sift_groundtruth[0].end(), std::ostream_iterator<float>(std::cout, " "));
+  // std::cout << std::endl;
+
+  shinx::HNSWIndex h;
+  return 0;
 }
